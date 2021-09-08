67a83d473396cf9314d1466e7e9ae87909452f56
gs20056 <82076678+gs20056@users.noreply.github.com>
2021-09-08 16:04:50 +0900

    OpenCV의 허프 변환에 대한 설명 추가

--- DetectMeteor/DetectMeteor.py
+++ DetectMeteor/DetectMeteor.py
@@ -6,7 +6,11 @@ minlight = 100
 #edges = cv2.Canny(src, 50, 150)
 ret, mask = cv2.threshold( src, minlight, 255, cv2.THRESH_BINARY)
 # 직선 성분 검출
+# 허프 변환을 통해 직선을 검출할 수 있는데, 그 함수는 2가지가 있으며, HoughLines보다 HoughLinesP 함수가 더 효율적 (부분 탐색을 하므로 짧은 시간 내에 가능)
 lines = cv2.HoughLinesP(mask, 1, np.pi / 180., 160, minLineLength=100, maxLineGap=30)
+# threshold: 일치해야 하는 점의 개수
+# minLineLength: 직선의 최소 길이
+# maxLineGap: 직선 간 최소 간격 : 구름을 검출할 경우, 이 값을 달리했을 때 직선의 개수가 크게 변한다. 반면, 맑은 하늘에 유성 하나 있는 경우는
 
 # 컬러 영상으로 변경 (영상에 빨간 직선을 그리기 위해)
 dst = cv2.cvtColor(src, cv2.COLOR_GRAY2BGR)
