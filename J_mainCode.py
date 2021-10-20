05d5de267d546446649892abb1e8fcc0fb714732
gs20036 <66413692+gs20036@users.noreply.github.com>
2021-10-20 11:42:18 +0900

    코드 99% 완성 날짜 자동으로 업데이트랑 실제 사용할 Code만 입력하면 100% 완성. 현재 밝기 평균값 21을 기준으로 나누는 중

--- J_mainCode.py
+++ J_mainCode.py
@@ -1,6 +1,19 @@
 import time, os, sys, cv2, numpy
 import FB107_utilities as FB
+import shutil
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
 
+processing_Date = '2021-10-04'
+save_Dir_Name = '../SAVE/'
+result_Dir_Name = '../result/'
+FB_Dir_Name = 'Fireball'
+Cloud_Dir_Name = 'Cloud'
+Plane_Dir_Name = 'Plane'
+Default_Dir_Name = 'Default'
+Get_Dir_Name = 'statistics_result_'
+
+Location_csv = "%s%s%s.csv" % (result_Dir_Name, Get_Dir_Name, processing_Date)
 
 # -------------------------------------------------------------------------
 def getCurrentDate():
@@ -67,7 +80,7 @@ def isFireBall(file):
 
 
 ###(유성인지 아닌지 판별)
-
+"""
 # 유성체감시네트워크에 적용될, 최종 유성 및 기타 물체 탐지 코드
 import numpy as np
 import cv2
@@ -80,14 +93,16 @@ absofD = cv2.mean(different)
 
 minlight = 100
 ret, mask = cv2.threshold(different, minlight, 255, cv2.THRESH_BINARY)
+"""
 
 
 # --------------------------------------------------------------------------
 def updateLog(data):
 # --------------------------------------------------------------------------
-    logDir = ".log.txt"
+    logDir = "../log.txt"
     f = open(logDir, 'a')
     f.write(data)
+    f.write('\n')
     f.close()
 
 # --------------------------------------------------------------------------
@@ -98,84 +113,94 @@ def moveFile(fromDir, toDir, fileName, code):
     To = "%s/%s" % (toDir, fileName)
 
     if not os.path.exists(toDir):
-        os.mkdir(toDir)
+        os.makedirs(toDir, exist_ok = True)
 
     try:
         fi = open(From, 'rb')
-        dat = fi.read().encode("hex")
+        dat = fi.read()
         fi.close()
-    except:
+    except Exception as err:
         updateLog("Error in opening file : [%s]" % From)
+        print(err)
         return
 
     try:
         fo = open(To, 'wb')
         fo.write(dat)
         fo.close()
-    except:
+    except Exception as err:
         updateLog("Error in writing filr : [%s]" % To)
         return
 
-    updateLog("%s, %s" % (fileName, code))
+    updateLog("Moved Successful!!! [%s], Code : %s" % (fileName, code))
+
+def init():
+    YYYYMMDDHHMISS = getYYYYMMDDHHMISS()
+    YYYYMMDD = YYYYMMDDHHMISS[:8]
+    HHMISS = YYYYMMDDHHMISS[8:]
+    updateLog("[%s-%s] : Running on %s" % (YYYYMMDD, HHMISS, Location_csv))
 
 
 # --------------------------------------------------------------------------
 def main():
 # --------------------------------------------------------------------------
 
-    save_Dir_Name = '../SAVE/'
-    result_Dir_Name = '../result/'
-    FB_Dir_Name = 'Fireball'
-    Cloud_Dir_Name = 'Cloud'
-    Plane_Dir_Name = 'Plane'
-    Default_Dir_Name = 'Default'
-    from datetime import datetime
-    from dateutil.relativedelta import relativedelta
-    processing_Date = '2021-10-04'
-    processing_Date1 = datetime.fromisoformat(processing_Date)
-    processing_Date2 = processing_Date1 + relativedelta(days = 1)
-    YYYY=processing_Date[:4]
-    MM=processing_Date[5:7]
-    DD=processing_Date[8:10]
 
 
-    processing_dir_names = [
-    "{}{:04d}/{:02d}/{:02d}/".format(save_Dir_Name, processing_Date1.year, processing_Date1.month, processing_Date1.day),
-    "{}{:04d}/{:02d}/{:02d}/".format(save_Dir_Name, processing_Date2.year, processing_Date2.month, processing_Date2.day)]
+    init()
 
-    N = input()
-    length = int(N)
+    f = open(Location_csv, 'rb')
+    dat = f.read()
+    Data = dat.decode().split('\r\n')
+    Data = Data[1:]
+
+    length = len(Data)-2
     infos = []
     for i in range(length):
-        A = input().split(',')
-        A[1]=A[1][1:]
-        infos.append([A[0][-1], A[1]])
-
+        Data[i] = Data[i].split(',')
+        print(Data[i])
     for i in range(length):
         try:
-            fileName = infos[i][0]
-            Code = infos[i][1]
+            fileName = Data[i][1]
+            Code = Data[i][4]
+            if int(float(Code)) > 20:
+                Code = 0
+            else:
+                Code = 1
             codeList = ["FB", "Default"]
             if int(Code) >= 0 and int(Code) < len(codeList):
                 code = codeList[int(Code)]
             else:
                 updateLog("Error in receiving code")
 
+            fileNames = fileName.split('-')
+            YYYY=fileNames[1][:4]
+            MM=fileNames[1][4:6]
+            DD=fileNames[1][6:8]
+            HH=fileNames[2][:2]
+            MI=fileNames[2][2:4]
+            SS=fileNames[2][4:6]
+            processing_Dir = "%s%s/%s/%s/%s" % (save_Dir_Name, YYYY, MM, DD, HH)
+            fileName = fileName.split("\\")
+            fileName = fileName[1]
+            print(fileName)
             if code == "FB":
-                moveFile(processing_Dir, "%s%s/%s/%s/%s" % (result_Dir_Name, FB_Dir_Name, YYYY, MM, DD), fileName, code)
+                moveFile(processing_Dir, "%s%s/%s/%s/%s/%s" % (result_Dir_Name, FB_Dir_Name, YYYY, MM, DD, HH), fileName, code)
             elif code == "Cloud":
-                moveFile(processing_Dir, "%s%s/%s/%s/%s" % (result_Dir_Name, Cloud_Dir_Name, YYYY, MM, DD), fileName,
+                moveFile(processing_Dir, "%s%s/%s/%s/%s/%s" % (result_Dir_Name, Cloud_Dir_Name, YYYY, MM, DD, HH), fileName,
                          code)
             elif code == "Plane":
-                moveFile(processing_Dir, "%s%s/%s/%s/%s" % (result_Dir_Name, Plane_Dir_Name, YYYY, MM, DD), fileName,
+                moveFile(processing_Dir, "%s%s/%s/%s/%s/%s" % (result_Dir_Name, Plane_Dir_Name, YYYY, MM, DD, HH), fileName,
                          code)
             elif code == "Default":
-                moveFile(processing_Dir, "%s%s/%s/%s/%s" % (result_Dir_Name, Default_Dir_Name, YYYY, MM, DD), fileName,
+                moveFile(processing_Dir, "%s%s/%s/%s/%s/%s" % (result_Dir_Name, Default_Dir_Name, YYYY, MM, DD, HH), fileName,
                          code)
             else:
                 updateLog("Error in receiving code")
         except Exception as err:
             print(err)
             continue
+
+    updateLog("\n\n")
 
 main()
