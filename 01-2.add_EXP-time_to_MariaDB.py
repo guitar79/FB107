c086b670237fc4f45945651a293bbde9a7fde29c
Kiehyun Kevin Park <guitar79@gs.hs.kr>
2022-04-04 15:40:27 +0900

    _ to -

--- 01-2.add_EXP-time_to_MariaDB.py
+++ 01-2.add_EXP-time_to_MariaDB.py
@@ -18,38 +18,50 @@ print ("err_log_file: {}".format(err_log_file))
 #########################################
 # mariaDB info
 #########################################
-SET_MariaDB = True
-if SET_MariaDB == True :
-    import pymysql
-    db_host = '192.168.0.20'
-    db_host = '10.114.0.120'
+import pymysql
+db_host = '192.168.0.20'
+db_host = '10.114.0.120'
 
-    db_user = 'modis'
-    db_pass = 'Modis12345!'
-    db_name = 'MODIS_Aerosol'
+db_user = 'modis'
+db_pass = 'Modis12345!'
+db_name = 'MODIS_Aerosol'
 
-    db_user = 'FB107'
-    db_pass = 'Gses12345!'
-    db_name = 'FB107'
+db_user = 'FB107'
+db_pass = 'Gses12345!'
+db_name = 'FB107'
 
-    table_hdf_info = 'SAVE_file_info'
+tb_name = 'SAVE_file_info'
 
-    conn = pymysql.connect(host=db_host, port=3306,
-                           user=db_user, password=db_pass, db=db_name,
-                           charset='utf8mb4', cursorclass=pymysql.cursors.DictCursor)
+conn = pymysql.connect(host=db_host, port=3306,
+                       user=db_user, password=db_pass, db=db_name,
+                       charset='utf8mb4', cursorclass=pymysql.cursors.DictCursor)
 
-    cur = conn.cursor()
+cur = conn.cursor()
 
-    q1 = """CREATE TABLE IF NOT EXISTS `{}`.`{}` (
+qry = "SHOW TABLES FROM `{}` LIKE '{}'".format(db_name, tb_name)
+
+table_exist = cur.execute(qry)
+
+if table_exist == 0:        
+    qry = """CREATE TABLE IF NOT EXISTS `{}`.`{}` (
         `id` INT NOT NULL AUTO_INCREMENT ,
         `fullname` VARCHAR(16384) default NULL ,
         `brightness-mean` VARCHAR(16) default NULL ,
         `brightness-std` VARCHAR(16) default NULL ,
+        `Line` VARCHAR(5) default NULL ,
     	`EXIF` TEXT default NULL ,
-        `Update-DT` TIMESTAMP on update CURRENT_TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ,
-        PRIMARY KEY (`id`)) ENGINE = InnoDB;""".format(db_name, table_hdf_info)
-    cur.execute(q1)
+    	`EXP_time` VARCHAR(10) NULL DEFAULT NULL AFTER `EXIF`;
+        `Update-DT` TIMESTAMP on update CURRENT_TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP , 
+        PRIMARY KEY (`id`)) ENGINE = InnoDB;""".format(db_name, tb_name)
+    cur.execute(qry)
     conn.commit()
+else :
+    qry = "SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA = '{}' AND TABLE_NAME = '{}';".format(db_name, tb_name)
+    cur.execute(qry)
+    colunm_dic = cur.fetchall()
+    print(colunm_dic)
+
+
 #########################################
 
 base_drs = ['../SAVE/']
